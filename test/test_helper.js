b13fd93f4f3b20b4f3953b0b3fc068149ff6dd41
Paul <paulr@freelearn.tv>
2017-06-30 15:47:26 +1200

    Add beforeEach() hook to drop instances within users_test

--- test/test_helper.js
+++ test/test_helper.js
@@ -7,4 +7,14 @@ mongoose.connection
     console.warn('Warning', error);
   });
 
+/*add hook initial function run before any test is run
+passing mochas 'done' into beforeEach to not run tests until data is dropped
+*/
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // ready to run the next test
+    done();
+  });
+});
+
   // to run, at cmd prompt "node test/test_helper.js"
