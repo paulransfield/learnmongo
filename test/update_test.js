47bae9d97536cdae1efaabd77b91e95933080417
Paul <paulr@freelearn.tv>
2017-07-01 10:56:43 +1200

    Refactor to add helper function assertName to keep code DRY

--- test/update_test.js
+++ test/update_test.js
@@ -11,19 +11,28 @@ describe('Updating records', () => {
       .then(() => done());
   });
 
-//testing 5 different ways of updating an instance with mongoose and mongo
-  it('instance type using set n save', (done) => {
-    console.log(joe);
-//using set property to update an instance in memory
-    joe.set('name', 'Alex');
-//to persist set change, call save
-    joe.save()
-//empty object {} to find all
+//define new helper function to remove copy and paste of .then code
+
+  function assertName(operation, done) {
+    operation
+    //empty object {} to find all
       .then(() => User.find({}))
       .then((users) => {
           assert(users.length === 1);
           assert(users[0].name === 'Alex');
           done();
-      });
-  });
+        });
+    }
+
+//testing 5 different ways of updating an instance with mongoose and mongo
+  it('instance type using set n save', (done) => {
+    //using set property to update an instance in memory
+    joe.set('name', 'Alex');
+    //to persist set change, call save
+    assertName(joe.save(), done);
+    });
+
+    it('model instance can update', (done) => {
+      assertName(joe.update({ name: 'Alex' }), done);
+    });
 });
