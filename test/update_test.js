4f82ae8b0f8b32595ba5ea96b16d0350b8069dd5
Paul <paulr@freelearn.tv>
2017-07-01 11:09:39 +1200

    Add it block tests for 3 easier methods of model class updates

--- test/update_test.js
+++ test/update_test.js
@@ -28,11 +28,35 @@ describe('Updating records', () => {
   it('instance type using set n save', (done) => {
     //using set property to update an instance in memory
     joe.set('name', 'Alex');
-    //to persist set change, call save
+    //to persist set change, call save - useful for piecemeal changes
     assertName(joe.save(), done);
     });
 
+    //update for making all changes in one operation
     it('model instance can update', (done) => {
       assertName(joe.update({ name: 'Alex' }), done);
+    });
+
+    //test using Class model updates
+    it('model class update', (done) => {
+      assertName(
+        User.update({ name: 'Joe' }, { name: 'Alex' }),
+        done
+      );
+    });
+    //test using Class model updates with findOneAndUpdate
+    it('model class can update one record', (done) => {
+      assertName(
+        User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }),
+        done
+      );
+    });
+
+    //test using Class model updates with findByIdAndUpdate
+    it('model class can find a record with an ID and update', (done) => {
+      assertName(
+        User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
+        done
+      );
     });
 });
