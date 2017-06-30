d1ef848424f7b9342940e67b82a4023379b3d2e8
Paul <paulr@freelearn.tv>
2017-07-01 08:47:57 +1200

    Add first it block to test removing an instance of joe

---
+++ test/delete_test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const User = require('../src/user.js');
+
+/* Four different ways to remove from mongo
+Model User class includes remove, findOneAndRemove, findByIdandRemove
+Model Joe instance includes remove
+*/
+
+describe('Deleting a user', () => {
+  "use strict";
+  let joe;
+
+  beforeEach((done) => {
+      joe = new User({ name: 'Joe'});
+      joe.save()
+        .then(() => done());
+  });
+
+  it('model instance remove', (done) => {
+    joe.remove()
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        assert(user === null);
+        done();
+      })
+  });
+
+  it('class model remove', () => {
+
+  });
+
+  it('class method findAndRemove', () => {
+
+  });
+
+  it('class method findByIdAndRemove', () => {
+
+  });
+});
