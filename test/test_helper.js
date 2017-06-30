52fac7e57fcbd20dfb7f485ae1b1122a41cda3a9
Paul <paulr@freelearn.tv>
2017-06-30 16:08:53 +1200

    Add before hook with function

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 // tell mongoose we are using ES6 promise library
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => {})
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+//add before hook to wait for mongodb connection before running tests
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
 /*add hook initial function run before any test is run
 passing mochas 'done' into beforeEach to not run tests until data is dropped
