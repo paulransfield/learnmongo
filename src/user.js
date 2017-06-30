1d56ecebbf638b81a7d206a432bc6ccec7ce9cf0
Paul <paulr@freelearn.tv>
2017-06-30 15:05:15 +1200

    Add section comments to code

--- src/user.js
+++ src/user.js
@@ -1,11 +1,16 @@
+//import statements
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//schema
 const UserSchema = new Schema({
   name: String
 });
 
+//model
 const User = mongoose.model('user', UserSchema);
 
+//export model
 module.exports = User;
 //model represents all of the data in a single mongodb
