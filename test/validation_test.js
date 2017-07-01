e976c748bea8708121ff5e2ced1613513f4c3723
Paul <paulr@freelearn.tv>
2017-07-01 12:24:34 +1200

    Add validation test to handle failed inserts to database using .catch

--- test/validation_test.js
+++ test/validation_test.js
@@ -4,8 +4,6 @@ const User = require('../src/user.js');
 describe('Validating records', () => {
   it('requires a user name', () => {
     const user = new User({ name: undefined });
-/* validateSync is a synchronous validation process -
-validation doesn't return a result */
     const validationResult = user.validateSync();
     const message = validationResult.errors.name.message;
     assert(message === 'Name is required.');
